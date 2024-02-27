648bfeea6e7b600d501237619734e478187b9f45
tada5hi <peter.placzek1996@gmail.com>
2024-02-27 11:25:21 +0100

    feat: implemented fundamental sdk for storage service

---
+++ packages/storage-kit/src/domains/bucket-file/api.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021-2024.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import type { BuildInput } from 'rapiq';
+import { buildQuery } from 'rapiq';
+import { nullifyEmptyObjectProperties } from '@privateaim/core';
+import type { CollectionResourceResponse, SingleResourceResponse } from '@privateaim/core';
+import type { BucketFile } from './entity';
+import { BaseAPI } from '../base';
+
+export class BucketFileAPI extends BaseAPI {
+    async getMany(record?: BuildInput<BucketFile>): Promise<CollectionResourceResponse<BucketFile>> {
+        const response = await this.client.get(`bucket-files${buildQuery(record)}`);
+        return response.data;
+    }
+
+    async getOne(id: BucketFile['id'], requestRecord?: BuildInput<BucketFile>): Promise<SingleResourceResponse<BucketFile>> {
+        const response = await this.client.get(`bucket-files/${id}${buildQuery(requestRecord)}`);
+
+        return response.data;
+    }
+
+    async create(data: Record<string, any>): Promise<SingleResourceResponse<BucketFile>> {
+        const response = await this.client.post('bucket-files', nullifyEmptyObjectProperties(data));
+
+        return response.data;
+    }
+
+    async delete(id: BucketFile['id']): Promise<SingleResourceResponse<BucketFile>> {
+        const response = await this.client.delete(`bucket-files/${id}`);
+        return response.data;
+    }
+
+    async update(id: BucketFile['id'], data: Record<string, any>): Promise<SingleResourceResponse<BucketFile>> {
+        const response = await this.client.post(`bucket-files/${id}`, nullifyEmptyObjectProperties(data));
+        return response.data;
+    }
+}
---
+++ packages/storage-kit/src/domains/bucket/api.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021-2024.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import type { BuildInput } from 'rapiq';
+import { buildQuery } from 'rapiq';
+import { nullifyEmptyObjectProperties } from '@privateaim/core';
+import type { CollectionResourceResponse, SingleResourceResponse } from '@privateaim/core';
+import { BaseAPI } from '../base';
+import type { Bucket } from './entity';
+
+export class BucketAPI extends BaseAPI {
+    async getMany(record?: BuildInput<Bucket>): Promise<CollectionResourceResponse<Bucket>> {
+        const response = await this.client.get(`buckets${buildQuery(record)}`);
+        return response.data;
+    }
+
+    async getOne(id: Bucket['id'], requestRecord?: BuildInput<Bucket>): Promise<SingleResourceResponse<Bucket>> {
+        const response = await this.client.get(`buckets/${id}${buildQuery(requestRecord)}`);
+
+        return response.data;
+    }
+
+    async create(data: Record<string, any>): Promise<SingleResourceResponse<Bucket>> {
+        const response = await this.client.post('buckets', nullifyEmptyObjectProperties(data));
+
+        return response.data;
+    }
+
+    async delete(id: Bucket['id']): Promise<SingleResourceResponse<Bucket>> {
+        const response = await this.client.delete(`buckets/${id}`);
+        return response.data;
+    }
+
+    async update(id: Bucket['id'], data: Record<string, any>): Promise<SingleResourceResponse<Bucket>> {
+        const response = await this.client.post(`buckets/${id}`, nullifyEmptyObjectProperties(data));
+        return response.data;
+    }
+}
