537d65081dee96b3a37cf413a481a6d9447806ba
tada5hi <peter.placzek1996@gmail.com>
2024-03-04 14:20:10 +0100

    feat: log analysis buckets configuration/destroying-process

--- packages/server-analysis-manager/src/components/core/commands/configure/module.ts
+++ packages/server-analysis-manager/src/components/core/commands/configure/module.ts
@@ -8,16 +8,27 @@
 import { BucketType } from '../../../../constants';
 import { useStorageClient } from '../../../../core';
 import { buildBucketName } from '../../../../helpers';
+import { CoreCommand } from '../../constants';
 import type { CoreConfigurePayload } from '../../type';
+import { useCoreLogger } from '../../utils';
 
 export async function executeCoreConfigureCommand(
     payload: CoreConfigurePayload,
 ) : Promise<CoreConfigurePayload> {
+    const logger = useCoreLogger();
     const storage = useStorageClient();
+
+    logger.debug('Creating analysis buckets...', {
+        command: CoreCommand.CONFIGURE,
+    });
 
     await storage.bucket.create({ name: buildBucketName(BucketType.FILES, payload.id) });
     await storage.bucket.create({ name: buildBucketName(BucketType.TEMP, payload.id) });
     await storage.bucket.create({ name: buildBucketName(BucketType.RESULTS, payload.id) });
+
+    logger.debug('Created analysis buckets...', {
+        command: CoreCommand.CONFIGURE,
+    });
 
     return payload;
 }
--- packages/server-analysis-manager/src/components/core/commands/destroy/module.ts
+++ packages/server-analysis-manager/src/components/core/commands/destroy/module.ts
@@ -8,16 +8,27 @@
 import { BucketType } from '../../../../constants';
 import { useStorageClient } from '../../../../core';
 import { buildBucketName } from '../../../../helpers';
+import { CoreCommand } from '../../constants';
 import type { CoreDestroyPayload } from '../../type';
+import { useCoreLogger } from '../../utils';
 
 export async function executeCoreDestroyCommand(
     payload: CoreDestroyPayload,
 ) : Promise<CoreDestroyPayload> {
+    const logger = useCoreLogger();
     const storage = useStorageClient();
+
+    logger.debug('Destroying analysis buckets...', {
+        command: CoreCommand.CONFIGURE,
+    });
 
     await storage.bucket.delete(buildBucketName(BucketType.FILES, payload.id));
     await storage.bucket.delete(buildBucketName(BucketType.TEMP, payload.id));
     await storage.bucket.delete(buildBucketName(BucketType.RESULTS, payload.id));
+
+    logger.debug('Destroyed analysis buckets...', {
+        command: CoreCommand.CONFIGURE,
+    });
 
     return payload;
 }
