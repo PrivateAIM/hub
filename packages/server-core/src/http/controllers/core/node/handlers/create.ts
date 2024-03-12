ed9d5962e17960d9d7550da950c7ad1e544e3ce5
tada5hi <peter.placzek1996@gmail.com>
2024-03-12 10:42:38 +0100

    feat: assign node to default registry on creation

--- packages/server-core/src/http/controllers/core/node/handlers/create.ts
+++ packages/server-core/src/http/controllers/core/node/handlers/create.ts
@@ -14,12 +14,11 @@ import { ForbiddenError } from '@ebec/http';
 import type { Request, Response } from 'routup';
 import { sendCreated } from 'routup';
 import { useDataSource } from 'typeorm-extension';
-import { RegistryCommand } from '../../../../../components';
-import { buildRegistryPayload } from '../../../../../components/registry/utils/queue';
+import { RegistryCommand, buildRegistryPayload } from '../../../../../components';
 import { hasAmqpClient, useAmqpClient } from '../../../../../core';
 import { useRequestEnv } from '../../../../request';
 import { createNodeRobot, runNodeValidation } from '../utils';
-import { NodeEntity, RegistryProjectEntity } from '../../../../../domains';
+import { NodeEntity, RegistryEntity, RegistryProjectEntity } from '../../../../../domains';
 
 export async function createNodeRouteHandler(req: Request, res: Response) : Promise<any> {
     const ability = useRequestEnv(req, 'ability');
@@ -36,7 +35,22 @@ export async function createNodeRouteHandler(req: Request, res: Response) : Prom
 
     // -----------------------------------------------------
 
+    let registryId : string | undefined;
     if (entity.registry_id) {
+        registryId = entity.registry_id;
+    } else {
+        const registryRepository = dataSource.getRepository(RegistryEntity);
+        // todo: default registry should be selected by unique identifying attribute
+        const registries = await registryRepository.find({
+            take: 1,
+        });
+        const [registry] = registries;
+        if (registry) {
+            registryId = entity.registry_id;
+        }
+    }
+
+    if (registryId) {
         const registryProjectExternalName = entity.external_name || createNanoID();
         entity.external_name = registryProjectExternalName;
 
@@ -46,7 +60,7 @@ export async function createNodeRouteHandler(req: Request, res: Response) : Prom
             name: entity.name,
             type: RegistryProjectType.NODE,
             realm_id: entity.realm_id,
-            registry_id: entity.registry_id,
+            registry_id: registryId,
             public: false,
         });
 
