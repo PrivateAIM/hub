14682ed4f52b7ea259d2cc8e214f4348073b9a10
Peter <peter.placzek1996@gmail.com>
2025-05-05 11:59:52 +0200

    feat: cleanup authup aggregator handlers (#1059)

--- packages/server-core/src/aggregators/authup/entities/robot.ts
+++ packages/server-core/src/aggregators/authup/entities/robot.ts
@@ -10,11 +10,16 @@ import type {
 } from '@authup/core-kit';
 import { ServiceID } from '@privateaim/core-kit';
 import { useDataSource } from 'typeorm-extension';
-import { useQueueRouter } from '@privateaim/server-kit';
+import { useLogger, useQueueRouter } from '@privateaim/server-kit';
 import { RegistryCommand, buildRegistryTaskQueueRouterPayload } from '../../../components';
 import { RegistryProjectEntity } from '../../../domains';
 
 export async function handleAuthupRobotEvent(context: EventRecord<EntityType.ROBOT, Robot>) {
+    if (!context.data.id) {
+        useLogger().warn('ID in authup robot event handler is missing.');
+        return;
+    }
+
     if (
         context.event !== 'created' &&
         context.event !== 'updated'
--- packages/server-core/src/aggregators/authup/entities/user.ts
+++ packages/server-core/src/aggregators/authup/entities/user.ts
@@ -6,9 +6,15 @@
  */
 import type { EntityType, EventRecord, User } from '@authup/core-kit';
 import { useDataSource } from 'typeorm-extension';
+import { useLogger } from '@privateaim/server-kit';
 import { AnalysisEntity, ProjectEntity } from '../../../domains';
 
 export async function handleAuthupUserEvent(context: EventRecord<EntityType.USER, User>) {
+    if (!context.data.id) {
+        useLogger().warn('ID in authup user event handler is missing.');
+        return;
+    }
+
     if (context.event === 'deleted') {
         const dataSource = await useDataSource();
 
