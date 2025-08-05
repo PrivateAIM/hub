045f3ba0bae085d0c1fc20f049193b4bbe91f40b
tada5hi <peter.placzek1996@gmail.com>
2025-08-05 11:11:12 +0200

    feat: cli and index entrpyoints

---
+++ packages/server-telemetry/src/components/event/adapter.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import { buildQueueRouterPublishPayload, isQueueRouterUsable, useQueueRouter } from '@privateaim/server-kit';
+import { EventEventQueueRouterRouting, EventTaskQueueRouterRouting } from './constants';
+import type { EventCommandContext, EventEventContext } from './types';
+
+export class EventComponentAdapter {
+    async command(ctx: EventCommandContext) {
+        if (isQueueRouterUsable()) {
+            const queueRouter = useQueueRouter();
+
+            const queueRouterPayload = buildQueueRouterPublishPayload({
+                type: ctx.command,
+                data: ctx.data,
+                metadata: {
+                    routing: EventTaskQueueRouterRouting,
+                },
+            });
+
+            await queueRouter.publish(queueRouterPayload);
+        }
+    }
+
+    async event(ctx: EventEventContext) {
+        if (isQueueRouterUsable()) {
+            const queueRouter = useQueueRouter();
+
+            const queueRouterPayload = buildQueueRouterPublishPayload({
+                type: ctx.event,
+                data: ctx.data,
+                metadata: {
+                    routing: EventEventQueueRouterRouting,
+                },
+            });
+
+            await queueRouter.publish(queueRouterPayload);
+        }
+    }
+}
