567a6e4616cfcd3a259b6a7213209269ed91a158
tada5hi <peter.placzek1996@gmail.com>
2026-03-02 11:17:03 +0100

    fix: remove middleware to transform basic to bearer authorization

--- packages/server-http-kit/src/middlewares/authup/middleware.ts
+++ packages/server-http-kit/src/middlewares/authup/middleware.ts
@@ -7,9 +7,8 @@
 
 import { createMiddleware } from '@authup/server-adapter-http';
 import { useRequestCookie } from '@routup/basic/cookie';
-import { parseAuthorizationHeader } from 'hapic';
 import type { Router } from 'routup';
-import { coreHandler, getRequestHeader } from 'routup';
+import { coreHandler } from 'routup';
 import type { AuthorizationMiddlewareRegistrationOptions } from './types.ts';
 import { applyTokenVerificationData, createFakeTokenVerificationData } from './utils.ts';
 
@@ -27,40 +26,6 @@ export function mountAuthorizationMiddleware(
 
         return;
     }
-
-    router.use(coreHandler(async (req, res, next) => {
-        const headerRaw = getRequestHeader(req, 'authorization');
-        if (typeof headerRaw !== 'string') {
-            next();
-        }
-
-        const cacheKey = `authorization-header:${headerRaw}`;
-
-        if (options.redisClient) {
-            const data = await options.redisClient.get(cacheKey);
-            if (data) {
-                req.headers.authorization = `Bearer ${data}`;
-                next();
-                return;
-            }
-        }
-
-        const header = parseAuthorizationHeader(headerRaw);
-
-        if (header.type === 'Basic') {
-            const token = await options.authupClient.token.createWithClientCredentials({
-                client_id: header.username,
-                client_secret: header.password,
-            });
-
-            req.headers.authorization = `Bearer ${token.access_token}`;
-            if (options.redisClient) {
-                await options.redisClient.setex(cacheKey, token.expires_in, token.access_token);
-            }
-        }
-
-        next();
-    }));
 
     const middleware = createMiddleware({
         tokenByCookie: (req, cookieName) => useRequestCookie(req, cookieName),
